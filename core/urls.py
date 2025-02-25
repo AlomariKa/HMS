62ba291e71f7c385033e00217e3c46de5b8b24e5
AlomariKa <omarikhaled1999@gmail.com>
2025-02-25 14:26:33 -0500

    Delete core directory

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('patient-dashboard/', views.patient_dashboard, name='patient_dashboard'),
-    path('doctor-dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
-    path('schedule-appointment/', views.schedule_appointment, name='schedule_appointment'),
-]
